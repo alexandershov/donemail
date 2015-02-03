62e0557742a723af47292da12a411d7ab3a389c5
aershov <codumentary.com@gmail.com>
2015-02-03 17:30:26 +0300

    Monkeypatch donemail.send_email in tests

--- tests/test_donemail.py
+++ tests/test_donemail.py
@@ -1,13 +1,27 @@
+import pytest
 import donemail
-
-# TODO: monkeypatch send_email
 
 BOB = 'bob@example.com'
 
 
+@pytest.fixture(autouse=True)
+def monkeypatch_send_email(monkeypatch):
+    monkeypatch.setattr('donemail.send_email', MockSendEmail())
+
+
+class MockSendEmail(object):
+    def __init__(self):
+        self.sent_emails = []
+
+    def __call__(self, to, subject, message):
+        self.sent_emails.append((to, subject, message))
+
+
 def test_context_manager():
+    assert len(donemail.send_email.sent_emails) == 0
     with donemail.donemail(BOB):
         pass
+    assert len(donemail.send_email.sent_emails) == 1
 
 
 def test_decorator():
@@ -15,4 +29,7 @@ def test_decorator():
     def add(x, y):
         return x + y
 
+    assert len(donemail.send_email.sent_emails) == 0
     add(1, 2)
+    assert len(donemail.send_email.sent_emails) == 1
+
