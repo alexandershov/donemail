101bf32703c4e75519816a2f33c4b0b97e3de1d3
aershov <codumentary.com@gmail.com>
2015-04-04 20:12:35 +0300

    Import donemail class into tests namespace
    So we don't need to constantly write donemail.donemail(BOB)
    Now we can just write donemail(BOB).

--- tests/test_donemail.py
+++ tests/test_donemail.py
@@ -1,12 +1,13 @@
 import pytest
-import donemail
+
+from donemail import donemail
 
 BOB = 'bob@example.com'
 
 
 @pytest.fixture(autouse=True)
 def monkeypatch_send_email(monkeypatch):
-    monkeypatch.setattr('donemail.donemail.send_email', MockSendEmail())
+    monkeypatch.setattr(donemail, 'send_email', MockSendEmail())
 
 
 class MockSendEmail(object):
@@ -19,17 +20,17 @@ class MockSendEmail(object):
 
 def test_context_manager():
     assert_num_emails(0)
-    with donemail.donemail(BOB):
+    with donemail(BOB):
         pass
     assert_num_emails(1)
 
 
 def assert_num_emails(expected_num_emails):
-    assert len(donemail.donemail.send_email.sent_emails) == expected_num_emails
+    assert len(donemail.send_email.sent_emails) == expected_num_emails
 
 
 def test_decorator():
-    @donemail.donemail(BOB)
+    @donemail(BOB)
     def add(x, y):
         return x + y
 
@@ -41,13 +42,13 @@ def test_decorator():
 def test_context_manager_with_exception():
     assert_num_emails(0)
     with pytest.raises(ZeroDivisionError):
-        with donemail.donemail(BOB):
+        with donemail(BOB):
             1 / 0
     assert_num_emails(1)
 
 
 def test_decorator_with_exception():
-    @donemail.donemail(BOB)
+    @donemail(BOB)
     def divide(x, y):
         return x / y
 
