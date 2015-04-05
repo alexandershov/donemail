1d06a9056afbd39cd3623b6b389be16af057a3b5
aershov <codumentary.com@gmail.com>
2015-04-05 21:38:43 +0300

    Test with tox
    Specify test requirements in tox.ini instead of setup(tests_require=[])

--- setup.py
+++ setup.py
@@ -6,10 +6,6 @@ setup(
     author='Alexander Ershov',
     version='0.1.0',
     packages=find_packages(),
-    tests_require=[
-        'mock',
-        'pytest',
-    ],
     entry_points={
         'console_scripts': [
             'donemail = donemail:main'
