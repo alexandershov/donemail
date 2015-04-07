393743e391575d6cf4a3bfffb4f53cfa0848c49e
aershov <codumentary.com@gmail.com>
2015-04-07 14:58:06 +0300

    Check that test emails are sent to BOB

--- tests/test_donemail.py
+++ tests/test_donemail.py
@@ -1,4 +1,4 @@
-from mock import Mock
+from mock import ANY, Mock
 import pytest
 import smtplib
 
@@ -9,18 +9,28 @@ BOB = 'bob@example.com'
 
 @pytest.fixture(autouse=True)
 def monkeypatch_send_email(monkeypatch):
-    monkeypatch.setattr('smtplib.SMTP', Mock())
+    mock_smtp_class = Mock()
+    mock_smtp_class.return_value = Mock()
+    monkeypatch.setattr('smtplib.SMTP', mock_smtp_class)
 
 
 def test_context_manager():
     assert_num_emails(0)
     with donemail(BOB):
         pass
-    assert_num_emails(1)
+    assert_sent_email(ANY, [BOB], ANY)
 
 
 def assert_num_emails(expected_num_emails):
-    assert smtplib.SMTP.return_value.sendmail.call_count == expected_num_emails
+    assert get_mock_smtp().sendmail.call_count == expected_num_emails
+
+
+def assert_sent_email(from_addr, to_addrs, msg):
+    get_mock_smtp().sendmail.assert_called_once_with(from_addr, to_addrs, msg)
+
+
+def get_mock_smtp():
+    return smtplib.SMTP()
 
 
 def test_decorator():
@@ -30,7 +40,7 @@ def test_decorator():
 
     assert_num_emails(0)
     add(1, y=2)
-    assert_num_emails(1)
+    assert_sent_email(ANY, [BOB], ANY)
 
 
 def test_context_manager_with_exception():
@@ -38,7 +48,7 @@ def test_context_manager_with_exception():
     with pytest.raises(ZeroDivisionError):
         with donemail(BOB):
             1 / 0
-    assert_num_emails(1)
+    assert_sent_email(ANY, [BOB], ANY)
 
 
 def test_decorator_with_exception():
@@ -49,5 +59,5 @@ def test_decorator_with_exception():
     assert_num_emails(0)
     with pytest.raises(ZeroDivisionError):
         divide(1, 0)
-    assert_num_emails(1)
+    assert_sent_email(ANY, [BOB], ANY)
 
